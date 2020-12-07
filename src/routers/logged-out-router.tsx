78ea5316711cd8f500ec09773553354dc7c7c8b0
nailseong <nnis9857@gmail.com>
2020-12-07 17:14:55 +0900

    React Hook Form

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,15 +1,57 @@
 import React from "react";
-import { isLoggedInVar } from "../apollo";
+import { useForm } from "react-hook-form";
+
+interface IForm {
+  email: string;
+  password: string;
+}
 
 export const LoggedOutRouter = () => {
-  const onClick = () => {
-    isLoggedInVar(true);
+  const { register, watch, handleSubmit, errors } = useForm<IForm>();
+  const onSubmit = () => {
+    console.log(watch());
+  };
+  const onInvalid = () => {
+    console.log("Can't create account");
   };
 
   return (
     <div>
       <h1>Logged out</h1>
-      <button onClick={onClick}>Click to login</button>
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div>
+          <input
+            ref={register({
+              required: "This is required",
+              pattern: /^[A-Za-z0-9]+@gmail.com$/,
+              validate: (email: string) => email.includes("@gmail.com"),
+            })}
+            name="email"
+            type="email"
+            placeholder="email"
+          />
+          {errors.email?.message && (
+            <span className="text-red-600 font-bold">
+              {errors.email?.message}{" "}
+            </span>
+          )}
+          {errors.email?.type === "pattern" && (
+            <span className="text-red-600 font-bold">Only Gmail Allowed</span>
+          )}
+        </div>
+        <div>
+          <input
+            ref={register({
+              required: "This is required",
+              validate: (password: string) => password.length >= 8,
+            })}
+            name="password"
+            type="password"
+            placeholder="password"
+          />
+        </div>
+        <button className=" bg-green-800 text-white">SUBMIT</button>
+      </form>
     </div>
   );
 };
